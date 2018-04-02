239766f4f39ce34e0d9f6f1e473cf6b3911f43bc
Thomas Latortue <tsl13b@gmail.com>
2018-04-01 20:45:08 -0400

    Add youtube API key

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
 import logo from './logo.svg';
 import './App.css';
 
+const API_KEY = 'AIzaSyCD1-o8OwZAuElbHT1_A_N8MUYJLHKShF8';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos : [],
+      selectedVideo : null
+    };
+
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo : videos[0]
+      });
+    });
+  }
+
   render() {
     return (
       <div className="App">
